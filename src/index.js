b692f1a656a9ee0bf40535fa6cab930811e6b7ac
Vitalii <morfius963@gmail.com>
2019-10-15 21:40:39 +0300

    Добавил папку для компонента

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MainPage from "./components/main-page.jsx";
+import MainPage from "./components/main-page/main-page.jsx";
 
 const init = () => {
   ReactDOM.render(
