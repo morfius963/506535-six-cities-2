2704f39cff3e3012bf8dc7e1d43853331183503e
Vitalii <morfius963@gmail.com>
2019-10-31 00:13:54 +0200

    Изменил название папки

--- src/__fixtures__/offers.js
+++ src/__fixtures__/offers.js
@@ -1,10 +1,21 @@
-const makeMockOffer = () => ({
+const coordinates = [
+  [52.3909553943508, 4.85309666406198],
+  [52.369553943508, 4.85309666406198],
+  [52.3909553943508, 4.929309666406198],
+  [52.3809553943508, 4.939309666406198]
+];
+
+const makeMockOffer = (coords) => ({
   title: `Beautiful &amp; luxurious apartment at great location`,
   isPremium: true,
   price: 250,
   rating: 4.5,
   type: `apartment`,
-  previewImage: `img/apartment-02.jpg`
+  previewImage: `img/apartment-02.jpg`,
+  location: {
+    coords,
+    zoom: 12
+  }
 });
 
-export default new Array(4).fill(``).map(makeMockOffer);
+export default coordinates.map((coord) => makeMockOffer(coord));
