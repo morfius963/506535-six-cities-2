72f04437f3eb18e3b29be8236660a4974e9b0c14
Vitalii <morfius963@gmail.com>
2019-10-30 21:16:29 +0200

    Изменил название папки на fixtures

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
-import fixtureData from "./__mocks__/offers.js";
+import fixtureData from "./__fixtures__/offers.js";
 
 const init = () => {
   ReactDOM.render(
