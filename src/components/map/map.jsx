021049379488b3bfa2fc12215065a7933d921643
Vitalii <morfius963@gmail.com>
2019-11-03 16:42:14 +0200

    Провёл рефакторинг кода компонента Карта

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -7,7 +7,7 @@ class Map extends React.PureComponent {
     super(props);
 
     this.DEFAULT_ZOOM = 12;
-    this.CITY_COORDS = [52.38333, 4.9];
+    this._map = null;
   }
 
   render() {
@@ -19,31 +19,44 @@ class Map extends React.PureComponent {
   componentDidMount() {
     const {offers} = this.props;
 
+    this._addMap(offers);
+  }
+
+  componentDidUpdate() {
+    const {offers} = this.props;
+
+    this._map.remove();
+    this._addMap(offers);
+  }
+
+  _addMap(offers) {
     const allOffersCoords = offers.map(({location}) => location.coords);
+    const cityCoords = offers[0].city.location;
 
     const icon = leaflet.icon({
       iconUrl: `./img/pin.svg`,
       iconSize: [30, 30]
     });
-    const map = leaflet.map(`map`, {
-      center: this.CITY_COORDS,
+
+    this._map = leaflet.map(`map`, {
+      center: cityCoords,
       zoom: this.DEFAULT_ZOOM,
       zoomControl: false,
       marker: true
     });
 
-    map.setView(this.CITY_COORDS, this.DEFAULT_ZOOM);
+    this._map.setView(cityCoords, this.DEFAULT_ZOOM);
 
     leaflet
       .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
         attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
       })
-      .addTo(map);
+      .addTo(this._map);
 
     allOffersCoords.forEach((offerCoord) => {
       leaflet
         .marker(offerCoord, {icon})
-        .addTo(map);
+        .addTo(this._map);
     });
   }
 }
