bbf20a6ba143bb9aeb86d479d5b780d41a0f766e
Vitalii <morfius963@gmail.com>
2019-11-10 04:19:06 +0200

    Обновил пропсы компонентов

--- src/components/offers-list/prop-types.js
+++ src/components/offers-list/prop-types.js
@@ -1,7 +1,12 @@
 import PropTypes from "prop-types";
 import offersPropTypes from "../app/prop-types.js";
+import {sortValues} from "../../__fixtures__/offers.js";
 
 export default {
   offers: PropTypes.arrayOf(offersPropTypes).isRequired,
-  activeCity: PropTypes.string.isRequired
+  activeCity: PropTypes.string.isRequired,
+  activeCardId: PropTypes.number.isRequired,
+  cardMouseEnterHandler: PropTypes.func.isRequired,
+  sortOffers: PropTypes.func.isRequired,
+  activeSort: PropTypes.oneOf(sortValues).isRequired
 };
