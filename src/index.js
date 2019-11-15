4ac3e483acf82827b7e009e5a5ac313d304cd682
Vitalii <morfius963@gmail.com>
2019-11-15 02:30:52 +0200

    Подключил библиотеку thunk

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {createStore, applyMiddleware, combineReducers} from "redux";
 import {Provider} from "react-redux";
+import {compose} from "recompose";
+import thunk from 'redux-thunk';
 
 import App from "./components/app/app.jsx";
-import {reducer} from "./reducer/reducer.js";
+import createAPI from "./api.js";
+import appData from "./store/reducers/app-data/app-data.js";
+import user from "./store/reducers/user/user.js";
 
 const init = () => {
+  const reducer = combineReducers({
+    appData,
+    user
+  });
+  const api = createAPI((...args) => store.dispatch(...args));
   const store = createStore(
       reducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+      compose(
+          applyMiddleware(thunk.withExtraArgument(api)),
+          window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+      )
   );
 
   ReactDOM.render(
