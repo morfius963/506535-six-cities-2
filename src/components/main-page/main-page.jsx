94e444a683dfcc34ae5aebe2d5a4c22d85c2fca6
Vitalii <morfius963@gmail.com>
2019-11-15 02:30:16 +0200

    Провёл небольшой рефакторинг

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -7,12 +7,9 @@ import MainHeader from "../main-header/main-header.jsx";
 import MainEmpty from "../main-empty/main-empty.jsx";
 import withActiveItem from "../../hocs/with-active-item/with-active-item.jsx";
 
-const MAX_CITIES_COUNT = 6;
 const OffersListWrapped = withActiveItem(OffersList);
 
-const MainPage = ({offers, activeOffers, city, onCityClick, sortOffers, activeSort}) => {
-  const allCities = Array.from(new Set(offers.map((offer) => offer.city.name))).slice(0, MAX_CITIES_COUNT);
-
+const MainPage = ({allCities, activeOffers, city, onCityClick, sortOffers, activeSort}) => {
   return <div className="page page--gray page--main">
 
     <MainHeader />
