8f1c4e84a05c04688211eb0e7c8ff3f7ea34e8ea
Vitalii <morfius963@gmail.com>
2019-11-23 14:58:30 +0200

    Обновил пропсы

--- src/components/offer-card/prop-types.js
+++ src/components/offer-card/prop-types.js
@@ -3,7 +3,6 @@ import offersPropTypes from "../app/prop-types.js";
 
 export default {
   offerData: offersPropTypes,
-  id: PropTypes.number.isRequired,
   onCardMouseEnter: PropTypes.func,
   onFavoriteCardToggle: PropTypes.func.isRequired,
   isInFavoriteList: PropTypes.bool
