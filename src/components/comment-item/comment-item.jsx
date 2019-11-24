f9594f23a020d3243f616992e7f5b1d3abbeaca1
Vitalii <morfius963@gmail.com>
2019-11-24 18:10:07 +0200

    Добавил возможность оставлять комментарии

--- src/components/comment-item/comment-item.jsx
+++ src/components/comment-item/comment-item.jsx
@@ -1,30 +1,50 @@
 import React from "react";
+import propTypes from "./prop-types.js";
 
-const CommentItem = () => {
+const CommentItem = ({commentData}) => {
+  const {user, rating, comment, date} = commentData;
+
+  const commentDate = new Date(date);
+  const commentMonth = commentDate.toLocaleString(`en-us`, {month: `long`});
+  const commentYear = commentDate.getFullYear();
+  const formattedDate = `${commentMonth} ${commentYear}`;
+
   return (
     <li className="reviews__item">
       <div className="reviews__user user">
         <div className="reviews__avatar-wrapper user__avatar-wrapper">
-          <img className="reviews__avatar user__avatar" src="../img/avatar-max.jpg" width={54} height={54} alt="Reviews avatar" />
+          <img className="reviews__avatar user__avatar" src="../img/avatar-user.-icon.jpg" width={54} height={54} alt="Reviews avatar" />
         </div>
-        <span className="reviews__user-name">
-          Max
+        <span className="reviews__user-name" style={{textAlign: `center`}}>
+          {user.name}
+          <br/>
+          {
+            user.isPro
+              ? <span className="property__user-status">
+                Pro
+              </span>
+              : null
+          }
         </span>
       </div>
       <div className="reviews__info">
         <div className="reviews__rating rating">
           <div className="reviews__stars rating__stars">
-            <span style={{width: `94%`}} />
+            <span style={{width: `${(rating * 100) / 5}%`}} />
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <p className="reviews__text">
-          A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
+          {comment}
         </p>
-        <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
+        <time className="reviews__time" dateTime={date}>
+          {formattedDate}
+        </time>
       </div>
     </li>
   );
 };
+
+CommentItem.propTypes = propTypes;
 
 export default CommentItem;
--- src/components/comments-list/comments-list.jsx
+++ src/components/comments-list/comments-list.jsx
@@ -1,12 +1,47 @@
 import React from "react";
+import {isEqual} from "lodash";
+import propTypes from "./prop-types.js";
 import CommentItem from "../comment-item/comment-item.jsx";
 
-const CommentsList = () => {
-  return (
-    <ul className="reviews__list">
-      <CommentItem />
-    </ul>
-  );
-};
+class CommentsList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this._MAX_COMMENTS_COUNT = 10;
+
+    this._loadComments();
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return !isEqual(this.props, nextProps);
+  }
+
+  componentDidUpdate() {
+    this._loadComments();
+  }
+
+  render() {
+    const {comments} = this.props;
+
+    return (
+      <ul className="reviews__list">
+        {
+          comments
+            .slice(0, this._MAX_COMMENTS_COUNT)
+            .sort((a, b) => a.date > b.date ? 1 : -1)
+            .map((comment) => <CommentItem key={`${comment.id}-${comment.rating}`} commentData={comment} />)
+        }
+      </ul>
+    );
+  }
+
+  _loadComments() {
+    const {id, onCommentsLoad} = this.props;
+
+    onCommentsLoad(id);
+  }
+}
+
+CommentsList.propTypes = propTypes;
 
 export default CommentsList;
