fc8155fe3b70611d057cd0fa1a61c49931b3a928
Vitalii <morfius963@gmail.com>
2019-11-28 00:40:10 +0200

    Удалил Redux Devtools

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import thunk from 'redux-thunk';
-import {createStore, applyMiddleware, combineReducers, compose} from "redux";
+import {createStore, applyMiddleware, combineReducers} from "redux";
 import {Provider} from "react-redux";
 import {Router} from "react-router-dom";
 
@@ -12,14 +12,6 @@ import user from "./store/reducers/user/user";
 import ActionCreator from "./store/actions/action-creator";
 import history from "./history";
 
-declare global {
-  interface Window { __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: () => any; }
-}
-
-const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-  : compose;
-
 const init = () => {
   const reducer = combineReducers({
     appData,
@@ -28,9 +20,7 @@ const init = () => {
   const api = createAPI(() => history.push(`/login`));
   const store = createStore(
       reducer,
-      composeEnhancers(
-          applyMiddleware(thunk.withExtraArgument(api))
-      )
+      applyMiddleware(thunk.withExtraArgument(api)),
   );
   const userData = localStorage.getItem(`userData`);
 
