08d92a23ec517e1f7742e364609025981e83bf01
Vitalii <morfius963@gmail.com>
2019-11-27 20:04:51 +0200

    Настроил окружение для работы с TypeScript

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   setupFilesAfterEnv: [`./jest.setup.js`],
-  snapshotSerializers: [`enzyme-to-json/serializer`]
+  snapshotSerializers: [`enzyme-to-json/serializer`],
+  transform: {
+    "^.+\\.tsx?$": `ts-jest`,
+    "^.+\\.js?$": `babel-jest`,
+  },
+  testRegex: `.test.(js?|jsx?|tsx?)$`,
+  moduleFileExtensions: [
+    `ts`,
+    `tsx`,
+    `js`,
+    `jsx`,
+    `json`,
+    `node`
+  ],
 };
