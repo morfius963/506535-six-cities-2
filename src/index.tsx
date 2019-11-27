553553acab91f5ef34dacbc4685d58fca96ea2bc
Vitalii <morfius963@gmail.com>
2019-11-27 20:22:31 +0200

    Изменил расширения файлов

--- src/index.tsx
+++ src/index.tsx
@@ -24,8 +24,8 @@ const init = () => {
   const store = createStore(
       reducer,
       compose(
-          applyMiddleware(thunk.withExtraArgument(api))
-          // __REDUX_DEVTOOLS_EXTENSION__ && __REDUX_DEVTOOLS_EXTENSION__()
+          applyMiddleware(thunk.withExtraArgument(api)),
+          __REDUX_DEVTOOLS_EXTENSION__ && __REDUX_DEVTOOLS_EXTENSION__()
       )
   );
   const userData = localStorage.getItem(`userData`);
