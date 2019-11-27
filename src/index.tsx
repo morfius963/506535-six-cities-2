f1da7bbb4ac6cf85950948fe2906b424875e55e0
Vitalii <morfius963@gmail.com>
2019-11-28 00:26:30 +0200

    Изменил настройки стора

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import thunk from 'redux-thunk';
-import {createStore, applyMiddleware, combineReducers} from "redux";
+import {createStore, applyMiddleware, combineReducers, compose} from "redux";
 import {Provider} from "react-redux";
-import {compose} from "recompose";
 import {Router} from "react-router-dom";
 
 import App from "./components/app/app";
@@ -13,7 +12,13 @@ import user from "./store/reducers/user/user";
 import ActionCreator from "./store/actions/action-creator";
 import history from "./history";
 
-declare const __REDUX_DEVTOOLS_EXTENSION__: () => any;
+declare global {
+  interface Window { __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: () => any; }
+}
+
+const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+  : compose;
 
 const init = () => {
   const reducer = combineReducers({
@@ -23,9 +28,8 @@ const init = () => {
   const api = createAPI(() => history.push(`/login`));
   const store = createStore(
       reducer,
-      compose(
-          applyMiddleware(thunk.withExtraArgument(api)),
-          __REDUX_DEVTOOLS_EXTENSION__ && __REDUX_DEVTOOLS_EXTENSION__()
+      composeEnhancers(
+          applyMiddleware(thunk.withExtraArgument(api))
       )
   );
   const userData = localStorage.getItem(`userData`);
